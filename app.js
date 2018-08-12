2ab8066d983aabc4268606b78bd48a78240daeea
StartFromThird <hzqweixin@163.com>
2018-08-12 23:32:44 +0800

    搭建http服务及 mock API

---
+++ app.js
@@ -0,0 +1,81 @@
+var koa = require('koa');
+var route = require('koa-route');
+var app = new koa();
+
+// 渲染模板
+var views = require('co-views')
+var render = views('./view', {
+  map: { html: 'ejs' }
+});
+
+// 模拟数据接口
+var service = require('./service/webAppService');
+// 调用 webAppService 方法，返回JSON
+var apitest = function(ctx) {
+    // 返回头设为不缓存
+    ctx.set('Cache-Control','no-cache');
+    ctx.response.body = service.get_test_data();    
+}
+app.use(route.get('/api_test', apitest));
+
+// 首页json
+// http://127.0.0.1:3001/ajax/leftNav
+var get_leftNav_json = function(ctx) {
+    ctx.set('Cache-Control','no-cache');
+    ctx.response.body = service.get_leftNav_data();    
+}
+app.use(route.get('/ajax/leftNav', get_leftNav_json));
+
+// http://127.0.0.1:3001/ajax/productBlock
+var get_productBlock_json = function(ctx) {
+    ctx.set('Cache-Control','no-cache');
+    ctx.response.body = service.get_productBlock_data();    
+}
+app.use(route.get('/ajax/productBlock', get_productBlock_json));
+
+// 商品详情页json get要获取参数
+// http://127.0.0.1:3001/ajax/goods?id=180807
+var querystring = require('querystring');
+var get_goods_json = function(ctx) {
+    ctx.set('Cache-Control', 'no-cache');
+    var params = querystring.parse(ctx.req._parsedUrl.query);
+    var id = params.id;
+    if(!id){
+        id = "";
+    }
+    ctx.body = service.get_goods_data(id);
+} 
+app.use(route.get('/ajax/goods', get_goods_json));
+
+// 静态文件目录
+// http://127.0.0.1:3001/js/common.js
+var path = require('path');
+var serve = require('koa-static');
+var staticPath = './static'
+var main = serve(path.join( __dirname,  staticPath));
+app.use(main);
+
+
+// 页面
+var koa_static = require('koa-static-server');
+app.use(route.get('/ejs_test', async function(ctx, next) {
+    ctx.set('Cache-Control', 'no-cache');
+    ctx.body = await render('test',{title:'title_test'});
+}));
+
+// 首页
+app.use(route.get('/', async function(ctx, next){
+    ctx.set('Cache-Control', 'no-cache');
+    ctx.body = await render('index', {title:'商城首页'});
+}));
+// 商品详情
+// http://127.0.0.1:3001/goodsDetail?id=180807
+app.use(route.get('/goodsDetail', async function(ctx, next){
+    ctx.set('Cache-Control', 'no-cache');
+    var params = querystring.parse(ctx.req._parsedUrl.query);
+    var goodsId = params.id;
+    console.log(goodsId);
+    ctx.body = await render('goodsDetail', {goodsId:goodsId});
+}));
+app.listen(3001);
+console.log('koa is started');
---
+++ service/webAppService.js
@@ -0,0 +1,62 @@
+
+// 提供方法 用于读取并返回数据
+// 模拟接口
+var fs = require('fs');
+exports.get_test_data = function(){
+    var content = fs.readFileSync('./mock/test.json','utf-8');
+    return content;
+}
+// 首页
+exports.get_leftNav_data = function(){
+    var content = fs.readFileSync('./mock/home/leftNav.json','utf-8');
+    return content;
+}
+exports.get_productBlock_data = function(){
+    var content = fs.readFileSync('./mock/home/productBlock.json','utf-8');
+    return content;
+}
+// 商品详情
+exports.get_goods_data = function(id){
+    if(!id){
+        id = "180817";
+    }
+    if(fs.existsSync('./mock/goods/' + id + '.json')){
+        return fs.readFileSync('./mock/goods/' + id + '.json', 'utf-8');
+    }else{
+        return fs.readFileSync('./mock/goods/180807.json', 'utf-8');
+    }
+    return content;
+}
+// 实际接口
+// exports.get_search_data = function(start, end, keyword){
+//     // 异步 接受数据后调用
+//     return function(cb){
+//         var http = require('http');
+//         var qs = require('querystring');
+//         var data = {
+//             s: keyword,
+//             start: start,
+//             end: end
+//         };
+//         var content = qs.stringify(data);
+//         var http_request = {
+//             hostname : 'dushu.xiaomi.com',
+//             port : 80,
+//             path : '/store/v0/lib/query/onebox?' + content
+//         }
+//         req_obj = http.request(http_request, function(_res){
+//             var content = '';
+//             _res.setEncoding('utf8');
+//             _res.on('data',function(chunk){
+//                 content += chunk;
+//             });
+//             _res.on('end', function(){
+//                 cb(null,content);
+//             });
+//         });
+//         req_obj.on('error', function(){
+
+//         });
+//         req_obj.end();
+//     }
+// }
